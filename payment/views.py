4ecb7566d0da7ef68b34c53b431641eb7de8ec9c
Jere-The-Hutt <jere.huttunen@live.com>
2025-07-09 14:57:28 +0200

    Add payment_process view with Stripe Checkout integration

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from decimal import Decimal
+import stripe
+from django.conf import settings
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from orders.models import Order
 
-# Create your views here.
+# create the Stripe instance
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    product = order.product
+
+    if request.method == 'POST':
+        success_url = request.build_absolute_uri(reverse('payment:completed'))
+        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
+
+        unit_amount = int(product.price * Decimal('100'))
+        # Stripe checkout session data
+        session_data = {
+            'mode': 'payment',
+            'client_reference_id': order.id,
+            'success_url': success_url,
+            'cancel_url': cancel_url,
+            'line_items': [{
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': product.name,
+                    },
+                    'unit_amount': unit_amount,
+                },
+                'quantity': 1,
+            }],
+        }
+        # create Stripe checkout session
+        session = stripe.checkout.Session.create(**session_data)
+        # redirect to Stripe payment form
+        return redirect(session.url, code=303)
+
+    return render(request, 'payment/process.html', {'order': order})
