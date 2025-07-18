6839892c79e8b0c25bd7a7083bc3b895ae78f21c
Jere-The-Hutt <jere.huttunen@live.com>
2025-07-18 13:10:13 +0200

    Register custom order request model in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, CustomPackageRequest
 
 
 @admin.register(Product)
@@ -15,3 +15,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(CustomPackageRequest)
+class CustomPackageRequestAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'message', 'created']
+    list_filter = ['created']
+    search_fields = ['name', 'email', 'subject']
+    readonly_fields = ['created']
+    ordering = ['-created']
